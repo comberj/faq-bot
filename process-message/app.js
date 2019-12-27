6014cdc5988e0f99676d22bcd7d4a22b41cdaf67
Kelton Kowalchuk <keltonkowalchuk@gmail.com>
2019-12-26 17:56:59 -0800

    feat: s3 uploading & policy created

--- process-message/app.js
+++ process-message/app.js
@@ -1,5 +1,6 @@
-// const axios = require('axios')
-// const url = 'http://checkip.amazonaws.com/';
+const aws = require('aws-sdk');
+const s3 = new aws.S3({ apiVersion: '2006-03-01' });
+
 let response;
 
 /**
@@ -16,31 +17,35 @@ let response;
  */
 exports.lambdaHandler = async (event, context) => {
     try {
-        // const ret = await axios(url);
-        // response = {
-        //     'statusCode': 200,
-        //     'body': JSON.stringify({
-        //         message: 'hello world',
-        //         // location: ret.data.trim()
-        //     })
-        // }
+        response = {
+            'isBase64Encoded': false,
+            'headers': {}
+        }
         const body = JSON.parse(event.body);
         if(body.type === 'url_verification') {
             console.log('url_verification');
-            response = {
+            Object.assign(response, {
                 'statusCode': 200,
-                'body': JSON.stringify({ challenge: body.challenge }),
-                'isBase64Encoded': false,
-                'headers': {}
-            };
+                'body': JSON.stringify({ challenge: body.challenge })
+            });
         } else if (body.type === 'event_callback'){
             console.log('event_callback');
-            response = {
-                'statusCode': 200,
-                'body': JSON.stringify(body),
-                'isBase64Encoded': false,
-                'headers': {}
+            
+            // TODO: Process keywords in message and store alongside body
+            // TODO: To generate link to previous message, take the following: https://winedirectteam.slack.com/archives/${body.event.channel}/p${body.event.event_ts.replace('.','')}
+            var params = {
+                Body: JSON.stringify(body), 
+                Bucket: process.env.BUCKET_NAME, 
+                Key: `${body.event.channel}/p${body.event.event_ts.replace('.','')}.json`
             };
+            console.log('params', params);
+            const s3Response = await s3.putObject(params).promise();
+            console.log('s3 response: ', s3Response);
+            
+            Object.assign(response, {
+                'statusCode': 200,
+                'body': JSON.stringify(body)
+            });
         };
     } catch (err) {
         console.log(err);
