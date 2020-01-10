5d4c199cd644052a0d1ac829932744bd3a408545
Kelton Kowalchuk <keltonkowalchuk@gmail.com>
2020-01-09 16:03:03 -0800

    feat: limit response to question to 3 results with at least 2 hits

--- methods/process-message/objects/events/QuestionEvent.js
+++ methods/process-message/objects/events/QuestionEvent.js
@@ -38,13 +38,16 @@ QuestionEvent.prototype.process = async function(){
 		let keywords = entities.Entities.concat(keyPhrases.KeyPhrases).map(function(k) { return k.Text });
 		this.body.keywords = keywords;
 
+		// Finds at most 3 related questions that have at least 2 matching keywords
 		const result_query = `
-			SELECT COUNT(id) as hits, message_ts, channel
-			FROM messages
-			WHERE keyword IN (${keywords.map(function (k) { return "'" + k.replace("'", "''") + "'"; }).join()})
-				AND channel = ?
-			GROUP BY message_ts, channel
-			ORDER BY hits DESC
+				SELECT COUNT(id) as hits, message_ts, channel
+				FROM messages
+				WHERE keyword IN (${keywords.map(function (k) { return "'" + k.replace("'", "''") + "'"; }).join()})
+					AND channel = ?
+				GROUP BY message_ts, channel
+				HAVING hits > 1
+				ORDER BY hits DESC
+				LIMIT 3
 		`;
 		let keywordResults = await mysql.query(result_query, [channel]);
 		console.log('Search Results:', keywordResults);
